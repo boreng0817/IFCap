068a33f3306619ee3042581d499bcedf3842bdb4
boreng0817 <boreng0817@naver.com>
2024-10-08 00:35:51 +0900

    feat: Modify download script

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-links=( "https://github.com/FeiElysia/ViECap/releases/download/checkpoints/evaluation.zip"
-    "https://github.com/FeiElysia/ViECap/releases/download/checkpoints/annotations.zip"
-    "https://github.com/FeiElysia/ViECap/releases/download/checkpoints/checkpoints.zip" )
+links=( "https://github.com/boreng0817/IFCap/releases/download/v1.0/evaluation.zip"
+    "https://github.com/boreng0817/IFCap/releases/download/v1.0/annotations.zip"
+    "https://github.com/boreng0817/IFCap/releases/download/v1.0/checkpoints.zip"
+    "https://github.com/boreng0817/IFCap/releases/download/v1.0/inference_result.zip" )
 
 for link in ${links[@]};
 do
     wget $link;
 done
 
-for f in annotations.zip checkpoints.zip evaluation.zip;
+for f in annotations.zip checkpoints.zip evaluation.zip inference_result.zip;
 do
     d=$(echo "${f%%.*}")
     unzip $f -d $d;
