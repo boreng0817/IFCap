3bc5ae0ff4163119b259f887ef5f2fb596abc17c
boreng0817 <boreng0817@naver.com>
2025-03-18 16:10:46 +0900

    feat: Add text feature extraction

--- src/texts_features_extraction.py
+++ src/texts_features_extraction.py
@@ -2,42 +2,41 @@ import os
 import clip
 import pickle
 import torch
-import json
 from tqdm import tqdm
+
 @torch.no_grad()
 def main(device: str, clip_type: str, inpath: str, outpath: str):
 
     device = device
     encoder, _ = clip.load(clip_type, device)
-    feat_rt_entities = {}
-    with open(inpath, 'r') as infile:
-        rt_entities = json.load(infile) # [[[entity1, entity2, ...], caption], ...]
 
-    batch_size = 256
-    for idx, (key, val) in tqdm(enumerate(rt_entities.items())):
-        # caption = rt_entities[1]
-        tokens = clip.tokenize(val, truncate = True).to(device)
+    with open(inpath, 'rb') as infile:
+        captions_with_entities = pickle.load(infile) # [[[entity1, entity2, ...], caption], ...]
+
+    for idx in tqdm(range(len(captions_with_entities))):
+        caption = captions_with_entities[idx][1]
+        tokens = clip.tokenize(caption, truncate = True).to(device)
         embeddings = encoder.encode_text(tokens).squeeze(dim = 0).to('cpu')
-        feat_rt_entities[key] = embeddings
-
+        captions_with_entities[idx].append(embeddings)
+    
     with open(outpath, 'wb') as outfile:
-        pickle.dump(feat_rt_entities, outfile)
-
-    return feat_rt_entities
+        pickle.dump(captions_with_entities, outfile)
+    
+    return captions_with_entities
 
 if __name__ == '__main__':
-
-    idx = 0 # change here! 0 -> coco training data, 1 -> flickr30k training data
-    device = 'cuda:0'
+    
+    idx = 1 # change here! 0 -> coco training data, 1 -> flickr30k training data
+    device = 'cuda:1'
     clip_type = 'ViT-B/32' # change here for different clip backbone (ViT-B/32, RN50x4)
     clip_name = clip_type.replace('/', '')
 
     inpath = [
-    'annotations/coco/rt_add_noise_image_caps_train.json',
-    'annotations/flickr30k/flickr30k_with_entities.pickle']
+    './annotations/coco/coco_with_entities.pickle',
+    './annotations/flickr30k/flickr30k_with_entities.pickle']
     outpath = [
-    f'annotations/coco/coco_rt_add_noise_texts_features.pickle',
-    f'annotations/flickr30k/flickr30k_texts_features_{clip_name}.pickle']
+    f'./annotations/coco/coco_texts_features_{clip_name}.pickle',
+    f'./annotations/flickr30k/flickr30k_texts_features_{clip_name}.pickle']
 
     if os.path.exists(outpath[idx]):
         with open(outpath[idx], 'rb') as infile:
@@ -48,7 +47,8 @@ if __name__ == '__main__':
     import random
     print(f'datasets for {inpath[idx]}')
     print(f'The length of datasets: {len(captions_with_features)}')
-    caption_with_features = captions_with_features[random.randint(0, len(captions_with_features) - 1)]
+    idx = random.randint(0, len(captions_with_features) - 1)
+    caption_with_features = captions_with_features[idx]
     detected_entities, caption, caption_features = caption_with_features
     print(detected_entities, caption, caption_features.size(), caption_features.dtype)
 
@@ -56,4 +56,4 @@ if __name__ == '__main__':
     with torch.no_grad():
         embeddings = encoder.encode_text(clip.tokenize(caption, truncate = True).to(device)).squeeze(dim = 0).to('cpu')
     print(abs(embeddings - caption_features).mean())
-
+    print(idx)
